e1c0393db52b4cef6a8a06a289182ff4dec104a6
Pratyay Dhond <dhondpratyay@gmail.com>
2022-05-23 18:27:23 +0530

    Updated filter - Working now YAYYYYY!!!

--- src/views/SearchPage/SearchResult.vue
+++ src/views/SearchPage/SearchResult.vue
@@ -1,9 +1,11 @@
 <script>
-import Header from '../../components/Header/Header.vue';
+// import Header from '../../components/Header/Header.vue';
 import Filter from '../../components/Filter/Filter.vue';
 import LatestBooks from '../../components/HomePage/LatestBooks.vue';
 import ResultBook from '../../components/Search/ResultBook.vue';
 import firebase from 'firebase/compat/app';
+
+
 export default{
     
     data(){
@@ -85,7 +87,7 @@ export default{
             books: [],
             filteredBooks:[],
             search: this.$route.query.search,
-            userID: this.$route.query.userID
+            userID: this.$route.query.userId
         }
     },
     created(){
@@ -103,19 +105,13 @@ export default{
                     obj.id = doc.id;
                     this.books.push(obj);
                 });
-                
-                this.books.forEach(
-                    function(item){
-                        if(item.name.includes(str))
-                            console.log(item)
-                    }
-                )
-
-                // for(let item in this.books){
-                //     if(item.name.includes(search.trim()))
-                //         console.log(item)
-                // }
                 console.log(this.books);
+                
+                // for(let i =0; i < this.books.length; i++){
+                    // if(this.books[i].name.includes(this.search))
+                        // console.log(this.book[i].name)
+                // }
+                this.filterData();
             })
         }
 
@@ -126,14 +122,28 @@ export default{
         }
     },
     components:{
-    Header,
     Filter,
     LatestBooks,
     ResultBook
 },
 methods:{
     submit(){
-        this.$route.go();
+
+    },
+    filterData(){
+        this.filteredBooks = []
+        for(let i = 0; i < this.books.length; i++){
+            if(this.books[i].name.toLowerCase().includes(this.search.toLowerCase().trim())){
+                this.filteredBooks.push(this.books[i]);
+            }
+        }
+        console.log(this.filteredBooks)
+    }
+},
+watch:{
+    search(){
+        console.log(this.search)
+        this.filterData();
     }
 }
 }
@@ -143,7 +153,51 @@ methods:{
     <div class="relative" >
         <!-- Header -->
         <div class="">
-            <Header :userID="userID" class="fixed z-10 w-full top-0" />
+               <div class="pt-3 px-5 pb-8 flex items-center justify-between bg-white">
+       
+       <router-link to="/home">
+        <div class="">
+            <img class="h-14" src="../../assets/images/logo.png" alt="" >
+        </div>
+        </router-link>
+        
+      
+        <div class="w-4/5">
+            <!-- <router-link to="/searchResult"> -->
+    <!-- <router-link :to="{name: 'searchResult', query: {userID: userID}}"> -->
+            <div class="relative" >
+                <svg class="pointer-events-none w-8 h-5 absolute top-1/2 transform -translate-y-1/2 left-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 90 90" width="90px" height="80px" @click="true">
+                  <g id="surface1552061">
+                     <path style=" stroke:none;fill-rule:nonzero;fill:rgb(61.176473%,63.921571%,68.627453%);fill-opacity:1;" d="M 39 9 C 22.46875 9 9 22.46875 9 39 C 9 55.53125 22.46875 69 39 69 C 46.191406 69 52.792969 66.445312 57.96875 62.207031 L 75.878906 80.121094 C 76.632812 80.90625 77.75 81.21875 78.800781 80.945312 C 79.851562 80.671875 80.671875 79.851562 80.945312 78.800781 C 81.21875 77.75 80.90625 76.632812 80.121094 75.878906 L 62.207031 57.964844 C 66.445312 52.792969 69 46.191406 69 39 C 69 22.46875 55.53125 9 39 9 Z M 39 15 C 52.289062 15 63 25.710938 63 39 C 63 52.289062 52.289062 63 39 63 C 25.710938 63 15 52.289062 15 39 C 15 25.710938 25.710938 15 39 15 Z M 39 15 "/>
+                  </g>
+                </svg>
+            <input class="bg-primary-1 appearance-none border-transparent border-white rounded-xl h-10 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-secondary-1 focus:ring-transparent pl-14 opacity-100" type="text" placeholder="Search for books" v-model="search" @keypress.enter="filterData">
+        </div>
+<!-- </router-link> -->
+        </div>
+        <div class="">
+            <div class="flex gap-8">
+        <router-link :to="{name: 'cart', query: {id: userID}}">
+        <div class="w-8" @click="true">
+            <img class="w-full" src="../../assets/images/cartBlack.png" alt="">
+        </div>
+        </router-link>
+
+        <router-link :to="{name: 'wishList', query: {id: userID}}">
+        <div class="w-8">
+            <img class="w-full" src="../../assets/images/heartBlack.png" alt="">
+        </div>
+        </router-link>
+
+        <router-link :to="{name: 'userView', query: {id: userID}}">
+        <div class="w-8">
+            <img class="w-full" src="../../assets/images/userBlack.png" alt="">
+        </div>
+        </router-link>
+    </div>
+        </div>
+        
+    </div>
         </div>
         
         <!-- Filter -->
@@ -164,7 +218,7 @@ methods:{
             </div>
             
             <div class="w-240" >
-                <div v-for="item in books" :key="item.id">
+                <div v-for="item in filteredBooks" :key="item.id">
                     <ResultBook  :bookName="item.name" :bookDate="item.publicationYear" bookLanguage="English" bookCover="hardcover" :bookPrice="item.price" :bookImageURL="item.url" :id="item.id"  :userID="userID"/>
             </div>
             </div>
